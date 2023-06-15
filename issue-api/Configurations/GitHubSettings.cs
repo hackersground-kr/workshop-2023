b32d3f115c56977dc6dea687774424dbb4e48f87
Justin Yoo <justin.yoojh@gmail.com>
2023-06-16 00:54:51 +0900

    Update app settings

--- issue-api/Configurations/GitHubSettings.cs
+++ issue-api/Configurations/GitHubSettings.cs
@@ -5,5 +5,7 @@ namespace WebApi.Configurations
         public const string Name = "GitHub";
 
         public virtual string? Agent { get; set; }
+        public virtual string? ClientId { get; set; }
+        public virtual string? ClientSecret { get; set; }
     }
 }
