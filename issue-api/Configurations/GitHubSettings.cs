f5da023a9ca52c985ec32c769c6d59e07144ca41
Justin Yoo <justin.yoojh@gmail.com>
2023-06-17 10:00:46 +0900

    Update Issue API config

--- issue-api/Configurations/GitHubSettings.cs
+++ issue-api/Configurations/GitHubSettings.cs
@@ -5,7 +5,5 @@ namespace WebApi.Configurations
         public const string Name = "GitHub";
 
         public virtual string? Agent { get; set; }
-        public virtual string? ClientId { get; set; }
-        public virtual string? ClientSecret { get; set; }
     }
 }
